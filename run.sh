b7083ba279c261346cbceacd2c76f614b95293e2
김상기/인프라플랫폼개발팀/CL <sangki.kim@nhn.com>
2024-05-30 16:23:38 +0900

    fix: run.sh 수정

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-docker build -t my-php-app1:latest . --no-cache
+docker build -t my-php-app1 . --no-cache
 
-docker run -it --rm -p 8081:80 -p 8080:443 -p 8080:443/udp -e CADDY_GLOBAL_OPTIONS=debug -e --name my-running-app my-php-app1:latest
+docker run -it --rm -p 80:80 -p 8081:443 -p 8081:443/udp -e CADDY_GLOBAL_OPTIONS=debug --name my-running-app my-php-app1
